67578e11631b16161a91182667d858761a95410c
PisecesPeng <Piseces.Peng@gmail.com>
2018-07-26 10:35:18 +0800

    update something

--- pp-ftp4node/run.sh
+++ pp-ftp4node/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-username=`whoami`
-if [ ! -d "/home/$username/Data/ftp4node" ]; then
-    mkdir -p /home/$username/Data/ftp4node
+#username=`whoami`
+if [ ! -d "~/Data/ftp4node" ]; then
+    mkdir -p ~/Data/ftp4node
 fi
 
 # node
 tar zxvf ./node-v8.11.3-linux-x64.tar.gz
+tar zxvf ./node_src.tar.gz
 cd ./node_src/
 npm install
 
@@ -16,5 +17,5 @@ cd ./../
 docker build -t pp-ftp4node .
 
 # docker run
-docker run -d -v /home/$username/Data/ftp4node/:/home/ftp4node/ -p 21:21 -p 3000:3000 -p 30000-30009:30000-30009 --restart=always --name pp-ftp4node pp-ftp4node
+docker run -d -v ~/Data/ftp4node/:/home/ftp4node/ -p 21:21 -p 3000:3000 -p 30000-30009:30000-30009 --restart=always --name pp-ftp4node pp-ftp4node
 
