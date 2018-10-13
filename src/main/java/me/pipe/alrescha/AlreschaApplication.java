006fb94bd4141c16686036350f158d027e42defc
PisecesPeng <Piseces.Peng@gmail.com>
2018-10-13 14:32:10 +0800

    create springboot

---
+++ src/main/java/me/pipe/alrescha/AlreschaApplication.java
@@ -0,0 +1,12 @@
+package me.pipe.alrescha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlreschaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlreschaApplication.class, args);
+    }
+}
---
+++ src/main/java/me/pipe/alrescha/controller/IndexController.java
@@ -0,0 +1,14 @@
+package me.pipe.alrescha.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "this is index";
+    }
+
+}
