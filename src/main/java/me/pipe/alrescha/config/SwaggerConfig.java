ef19d55c056165c0b753082ad0386268f6f96592
PisecesPeng <Piseces.Peng@gmail.com>
2018-10-13 15:18:21 +0800

    add swagger

---
+++ src/main/java/me/pipe/alrescha/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package me.pipe.alrescha.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/swagger/**").addResourceLocations("classpath:/static/swagger/");
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)) //加了ApiOperation注解的方法，生成接口文档
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Alrescha")
+                .description("\"Alrescha APIs Docs\"")
+                .version("0.0.1")
+                .build();
+    }
+
+}
--- src/main/java/me/pipe/alrescha/controller/IndexController.java
+++ src/main/java/me/pipe/alrescha/controller/IndexController.java
@@ -1,14 +1,22 @@
 package me.pipe.alrescha.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/index")
+@Api(value="index-controller", description="this is index controller")
 public class IndexController {
 
-    @GetMapping("/index")
-    public String index() {
-        return "this is index";
+    @GetMapping("/getInfo")
+    @ApiOperation("返回获得的str信息")
+    public String index(@RequestParam(value="str1", required=true) @ApiParam("用户输入的str1(必填)") String str1, @RequestParam(value="str2", required=false) @ApiParam("用户输入的str2") String str2) {
+        return "index get str1 : " + str1 + ";\nindex get str2 : " + str2 + ";";
     }
 
 }
