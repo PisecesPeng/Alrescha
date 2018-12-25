93294b62476b38abfd618da4c815d1a7f9d32db3
PisecesPeng <Piseces.Peng@gmail.com>
2018-12-25 19:22:52 +0800

    update redis

--- pp-redis/run.sh
+++ pp-redis/run.sh
@@ -5,5 +5,4 @@ set -e
 docker build -t pp-redis .
 
 # docker run
-docker run -d -p 6379:6379 --restart=always --name pp-redis pp-redis
-
+docker run -d -p 6379:6379 --restart=always --name pp-redis pp-redis redis-server --requirepass "q1w2e3r4"
